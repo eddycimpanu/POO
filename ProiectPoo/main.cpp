5ce7ecd70f26e1feb7eb11303dd7033a1c24d738
eddycimpanu <eddy.cimpanu14@gmail.com>
2025-05-29 17:20:50 +0300

    Add files via upload

--- ProiectPoo/main.cpp
+++ ProiectPoo/main.cpp
@@ -4,5 +4,10 @@ int main()
 {
     Pizzerie pizzerie; //instantiere obiect pizzerie
     pizzerie.run(); // pornim programul efectiv
+    
+    /*
+    g++ main.cpp produs.cpp pizza.cpp userinterface.cpp ingredient.cpp pizza_margherita.cpp pizza_diavola.cpp comanda.cpp managercomenzi.cpp pizza_quattro_carne.cpp pizza_kebab.cpp pizzerie.cpp pizza_quattro_stagioni.cpp pizza_quattro_formaggi.cpp  -o program.exe 
+    comanda pentru a rula
+    */
     return 0; 
 }
